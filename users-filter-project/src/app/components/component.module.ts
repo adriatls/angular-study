f8d52e78ae6975456f209a185b4b6549878f3018
adriatls <adria.tavares28@gmail.com>
2024-07-22 15:10:20 -0300

    fear: created filter component

--- users-filter-project/src/app/components/component.module.ts
+++ users-filter-project/src/app/components/component.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { UserDetailsComponent } from './user-details/user-details.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
     UserDetailsComponent,
+    FilterComponent,
   ],
   imports: [CommonModule, AngularMaterialModule],
-  exports: [UserDetailsComponent],
+  exports: [UserDetailsComponent, FilterComponent],
 })
 export class ComponentModule {}
