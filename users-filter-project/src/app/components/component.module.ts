b9b081cdb0eda879de8f871ceedaffba2503557f
adriatls <adria.tavares28@gmail.com>
2024-07-22 14:42:37 -0300

    feat: added static user-details

--- users-filter-project/src/app/components/component.module.ts
+++ users-filter-project/src/app/components/component.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    UserDetailsComponent,
+  ],
   imports: [CommonModule, AngularMaterialModule],
-  exports: [],
+  exports: [UserDetailsComponent],
 })
 export class ComponentModule {}
