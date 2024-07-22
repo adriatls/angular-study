6c8b931d511e0e0498e5b5d1361f916517d264de
adriatls <adria.tavares28@gmail.com>
2024-07-22 17:43:49 -0300

    fear: created users-list component

--- users-filter-project/src/app/components/component.module.ts
+++ users-filter-project/src/app/components/component.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { FilterComponent } from './filter/filter.component';
+import { UsersListComponent } from './users-list/users-list.component';
 
 @NgModule({
   declarations: [
     UserDetailsComponent,
     FilterComponent,
+    UsersListComponent,
   ],
   imports: [CommonModule, AngularMaterialModule],
-  exports: [UserDetailsComponent, FilterComponent],
+  exports: [UserDetailsComponent, FilterComponent, UsersListComponent],
 })
 export class ComponentModule {}
