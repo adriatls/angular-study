a319383300aa689443b10202294cf760816c178d
adriatls <adria.tavares28@gmail.com>
2024-07-22 18:29:20 -0300

    feat: added output when user clicks at a user

--- users-filter-project/src/app/app.component.ts
+++ users-filter-project/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IUser } from './interfaces/user/user.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  selectedUser: IUser = {} as IUser;
+
+  onUserSelected(emmitedValue: IUser) {
+    this.selectedUser = emmitedValue;
+  }
 }
