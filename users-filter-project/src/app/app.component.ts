e6ac54078fa6e0a88c418697ed2e679d1776a24f
adriatls <adria.tavares28@gmail.com>
2024-07-23 15:39:42 -0300

    feat: displyed user details conditionaly

--- users-filter-project/src/app/app.component.ts
+++ users-filter-project/src/app/app.component.ts
@@ -8,8 +8,10 @@ import { IUser } from './interfaces/user/user.interface';
 })
 export class AppComponent {
   selectedUser: IUser = {} as IUser;
+  showUserDetails = false;
 
   onUserSelected(emmitedValue: IUser) {
     this.selectedUser = emmitedValue;
+    this.showUserDetails = true;
   }
 }
